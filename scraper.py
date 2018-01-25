7b1bdc510e64b52f75adaaedd2fdf382aa442eb3
Paul Bradshaw <paulonhismobile@gmail.com>
2018-01-25 13:21:03 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -6,12 +6,16 @@ import lxml.html
 #
 # # Read in a page
 html = scraperwiki.scrape("https://uk.soccerway.com/teams/france/racing-club-de-strasbourg/898/")
-#
+
+record = {}
+
 # # Find something on the page using css selectors
 root = lxml.html.fromstring(html)
 names = root.cssselect("td div a")
 for name in names:
   print name.attrib['href']
+  record['link'] = name.attrib['href']
+  scraperwiki.sqlite.save(unique_keys=['link'], data=record)
 #
 # # Write out to the sqlite database using scraperwiki library
 # scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
